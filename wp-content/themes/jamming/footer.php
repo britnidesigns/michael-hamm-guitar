9118c997bcaeec5078b5e126204bc336a455d835
Britni Fletcher <me@britnidesigns.com>
2017-08-26 18:08:00 -0700

    Base styles, fonts, navigation, added footer menu to template

--- wp-content/themes/jamming/footer.php
+++ wp-content/themes/jamming/footer.php
@@ -14,7 +14,14 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
+        <?php
+            wp_nav_menu( array(
+                'theme_location' => 'menu-2',
+                'menu_class'     => 'sitemap',
+            ) );
+        ?>
+        <p>&copy; 2017 Michael Hamm</p>
+		<!-- <div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jamming' ) ); ?>"><?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'jamming' ), 'WordPress' );
@@ -24,7 +31,7 @@
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'jamming' ), 'jamming', '<a href="http://underscores.me/">Underscores.me</a>' );
 			?>
-		</div><!-- .site-info -->
+		</div> --><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
